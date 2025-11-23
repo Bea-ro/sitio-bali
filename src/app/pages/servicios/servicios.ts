e62731a0ce2cf63ae930506bc25269b74eadd26d
Beatriz Rodríguez <116880473+Bea-ro@users.noreply.github.com>
2025-11-24 00:38:11 +0100

    fix fragments navigation issue

--- src/app/pages/servicios/servicios.ts
+++ src/app/pages/servicios/servicios.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { Service } from '../../models/models';
 import { services } from '../../data/data';
 import { ImageAndText } from '../../shared/image-and-text/image-and-text';
 import { interval, map } from 'rxjs';
-import { CommonModule } from '@angular/common';
+import { CommonModule, ViewportScroller } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-servicios',
@@ -11,9 +12,24 @@ import { CommonModule } from '@angular/common';
   templateUrl: './servicios.html',
   styleUrl: './servicios.css',
 })
-export class Servicios {
+export class Servicios implements AfterViewInit {
   public services: Service[] = services;
   public asesorias: string[] = ['fiscal', 'contable', 'laboral', 'mercantil'];
 
   asesoria$ = interval(2000).pipe(map((i) => this.asesorias[i % this.asesorias.length]));
+
+  constructor(private route: ActivatedRoute, private scroller: ViewportScroller) {}
+  ngAfterViewInit() {
+    // Esperar a que Angular termine de renderizar
+    setTimeout(() => {
+      const fragment = this.route.snapshot.fragment;
+      if (fragment) {
+        const el = document.getElementById(fragment);
+        if (el) {
+          const y = el.getBoundingClientRect().top + window.scrollY - 118;
+          window.scrollTo({ top: y, behavior: 'smooth' });
+        }
+      }
+    }, 150);
+  }
 }

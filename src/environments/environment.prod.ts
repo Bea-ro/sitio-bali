a52dac71ae1a5a6b1fc71db11390d465ae73082c
Beatriz Rodríguez <116880473+Bea-ro@users.noreply.github.com>
2025-11-28 20:53:14 +0100

    config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   imagePrefix: 'https://res.cloudinary.com/dnlceaase/image/upload/f_auto,q_auto',
-  API_URL: '',
+  API_URL: _NGX_ENV_.NG_APP_API_URL,
 };

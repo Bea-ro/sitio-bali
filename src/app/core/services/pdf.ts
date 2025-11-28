5921eebf212624f3492b12fd2a4277978a93e581
Beatriz Rodríguez <116880473+Bea-ro@users.noreply.github.com>
2025-11-28 07:19:12 +0100

    save font

--- src/app/core/services/pdf.ts
+++ src/app/core/services/pdf.ts
@@ -1,38 +1,68 @@
 import { Injectable } from '@angular/core';
 import pdfMake from 'pdfmake/build/pdfmake';
-import pdfFonts from 'pdfmake/build/vfs_fonts';
+//import pdfFonts from 'pdfmake/build/vfs_fonts';
 
-(pdfMake as any).vfs = pdfFonts;
+//pdfMake.vfs = pdfFonts.vfs;
+
+(pdfMake as any).fonts = {
+  Garamond: {
+    normal: 'http://localhost:4200/assets/fonts/GARA.TTF',
+    bold: 'http://localhost:4200/assets/fonts/GARABD.TTF',
+  },
+};
 
 @Injectable({
   providedIn: 'root',
 })
 export class Pdf {
-  generatePdf(title: string, content: string) {
+  private logoBase64!: string;
+
+  async loadLogo(): Promise<string> {
+    const resp = await fetch('/bali-asociados-logo.webp');
+    const blob = await resp.blob();
+
+    // Convertir a PNG (que pdfMake sí soporta)
+    const bitmap = await createImageBitmap(blob);
+
+    const canvas = document.createElement('canvas');
+    canvas.width = bitmap.width;
+    canvas.height = bitmap.height;
+    const ctx = canvas.getContext('2d')!;
+    ctx.drawImage(bitmap, 0, 0);
+
+    return canvas.toDataURL('image/png'); // <- PNG compatible
+  }
+  async generatePdf(title: string, content: string) {
+    this.logoBase64 = await this.loadLogo();
     const documentDefinition: any = {
       pageSize: 'A4',
-      pageMargins: [40, 100, 40, 80], // izq, top, der, bottom
+      pageMargins: [60, 110, 60, 70], // izq, top, der, bottom
 
       // Cabecera
       header: {
-        margin: [40, 40],
+        margin: [60, 40],
         columns: [
           {
-            text: 'BALI ASOCIADOS EN GESTIÓN, S.L.',
-            alignment: 'left',
-            style: 'headerFirst',
-            margin: [0, 25, 0, 0],
+            width: '*',
+            stack: [
+              {
+                text: 'BALI ASOCIADOS EN GESTIÓN, S.L.',
+                alignment: 'left',
+                style: 'headerFirst',
+                margin: [0, 10, 0, 0],
+              },
+              {
+                text: 'Cl. Modesto Lafuente, 41 2º B Madrid (28.003)',
+                alignment: 'left',
+                style: 'headerSecond',
+              },
+            ],
           },
           {
-            text: 'Cl. Modesto Lafuente, 41 2º B Madrid (28.003)',
-            alignment: 'left',
-            style: 'headerSecond',
-            margin: [0, 25, 0, 0],
+            image: this.logoBase64,
+            width: 120,
+            alignment: 'right',
           },
-          // {
-          //   image: 'data:image/png;base64,...',
-          //   width: 120,
-          // },
         ],
       },
 
@@ -40,10 +70,11 @@ export class Pdf {
       footer: (currentPage: number, pageCount: number) => {
         return {
           margin: [40, 20],
-          columns: [
+          stack: [
             {
               text: 'Bali asociados, inscrita en el RM de Madrid al Tomo: 20.611, Folio: 67, Sección: 8ª Hoja: M364885, CIF: B84133586',
-              style: 'footerLeft',
+              style: 'footerCenter',
+              alignment: 'center',
             },
             {
               text: `${currentPage}/${pageCount}`,
@@ -60,15 +91,17 @@ export class Pdf {
       ],
 
       styles: {
-        headerFirst: { fontSize: 11, bold: true },
-        headerSecond: { fontSize: 11 },
-        footerLeft: { fontSize: 8 },
+        defaultStyle: {
+          fontSize: 11,
+          font: 'Garamond',
+        },
+        headerFirst: { fontSize: 11, bold: true, lineHeight: 1.5 },
+        footerCenter: { fontSize: 8, color: '#231473' },
         footerRight: { fontSize: 8 },
         title: { fontSize: 16, bold: true, margin: [0, 20, 0, 20] },
-        content: { fontSize: 11, lineHeight: 1.4 },
+        content: { fontSize: 11, lineHeight: 1.5 },
       },
     };
-
-    pdfMake.createPdf(documentDefinition).download(`${title}.pdf`);
+    pdfMake.createPdf(documentDefinition).open();
   }
 }

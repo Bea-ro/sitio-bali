1bf49d3c8e2b1d351120ec0e4bafa751aa88f9b0
Beatriz Rodríguez <116880473+Bea-ro@users.noreply.github.com>
2026-02-22 20:15:31 +0100

    add images to home banner

--- src/app/pages/home/components/home-banner/home-banner.ts
+++ src/app/pages/home/components/home-banner/home-banner.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { interval, map } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { interval, map, startWith, timer } from 'rxjs';
 import { environment } from '../../../../../environments/environment';
 
 @Component({
@@ -9,9 +9,30 @@ import { environment } from '../../../../../environments/environment';
   templateUrl: './home-banner.html',
   styleUrl: './home-banner.css',
 })
-export class HomeBanner {
+export class HomeBanner implements OnInit {
   public asesorias: string[] = ['fiscal', 'contable', 'laboral', 'mercantil'];
   public imagePrefix = environment.imagePrefix;
+  public images: string[] = [
+    this.imagePrefix + '/v1771784672/bali/bali-asociados-presentacion_1_kc6nux.webp',
+    this.imagePrefix + '/v1771784672/bali/bali-asociados-presentacion_3_aimtki.webp',
+    this.imagePrefix + '/v1771784672/bali/bali-asociados-presentacion_4_xsxaek.webp',
+    this.imagePrefix + '/v1771784672/bali/bali-asociados-presentacion_2_autogm.webp',
+  ];
 
-  asesoria$ = interval(2000).pipe(map((i) => this.asesorias[i % this.asesorias.length]));
+  public asesoriaIndex$ = interval(2000).pipe(
+    startWith(0),
+    map((i) => i % this.asesorias.length)
+  );
+
+  public image$ = interval(2000).pipe(
+    map((i) => this.images[i % this.images.length]),
+    startWith(this.images[0])
+  );
+
+  ngOnInit() {
+    this.images.forEach((src) => {
+      const img = new Image();
+      img.src = src;
+    });
+  }
 }
